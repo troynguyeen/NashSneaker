cce78e4ef91f65e5771714e11962b43555571964
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-10-21 19:30:35 +0700

    Add new CategoryMenu

---
+++ ASP.NET Core/Components/CategoryMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NashSneaker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NashSneaker.Views.Shared.Component
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private NashSneakerContext _context;
+
+        public CategoryMenuViewComponent(NashSneakerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categoryList = await _context.Category.ToListAsync();
+
+            return View(categoryList);
+        }
+    }
+}
