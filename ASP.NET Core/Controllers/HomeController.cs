0ef0ba349fde68917fef830498f7101f5c4d7f9f
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-10-24 14:55:16 +0700

    Update Delete item from Cart

--- ASP.NET Core/Controllers/HomeController.cs
+++ ASP.NET Core/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NashSneaker.Data;
 using NashSneaker.Models;
@@ -14,14 +16,31 @@ namespace NashSneaker.Controllers
     {
 
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<User> _userManager;
+        private NashSneakerContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, NashSneakerContext context)
         {
             _logger = logger;
+            _userManager = userManager;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            int counter = 0;
+            var userId = _userManager.GetUserId(User);
+            var _cart = _context.Cart.SingleOrDefault(cart => cart.User.Id == userId);
+            var _product = _context.Product.ToList();
+            var cartDetailList = _context.CartDetail.Where(detail => detail.Cart.User.Id == userId).ToList();
+
+            foreach (var item in cartDetailList)
+            {
+                counter += item.Quantity;
+            }
+
+            HttpContext.Session.SetInt32("CartCounter", counter);
+
             return View();
         }
 
