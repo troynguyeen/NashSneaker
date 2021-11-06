8905d1f89f089859214c7a69857fa0e1648a8513
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-11-06 23:35:48 +0700

    Create xUnit Test project and run with ShoppingControllerTests

---
+++ NashSneaker.UnitTest/ShoppingControllerTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NashSneaker.Controllers;
+using NashSneaker.Data;
+using System;
+using Xunit;
+
+namespace NashSneaker.UnitTest
+{
+    public class ShoppingControllerTests
+    {
+        [Fact]
+        public void Detail_WithProductHavingId10_ReturnsProductHavingId10()
+        {
+            //create In Memory Database
+            var options = new DbContextOptionsBuilder<NashSneakerContext>()
+            .UseInMemoryDatabase(databaseName: "NashSneaker")
+            .Options;
+
+            //// Create mocked Context by seeding Data as per Schema///
+
+            using (var context = new NashSneakerContext(options))
+            {
+                context.Product.Add(new Product
+                {
+                    Id = 10,
+                    Name = "Jordan 4 G",
+                    Category = new Category {Id = 1},
+                    Price = 6459000,
+                    Description = "Description for Air Jordan 4 G"
+                });
+
+                context.SaveChanges();
+            }
+
+            //Arrange
+            var mockContext = new NashSneakerContext(options);
+            var controller = new ShoppingController(mockContext);
+
+            //Action
+            var result = controller.Detail(10) as ViewResult;
+            var product = (Product) result.ViewData.Model;
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal(10, product.Id);
+            Assert.Equal("Jordan 4 G", product.Name);
+        }
+    }
+}
