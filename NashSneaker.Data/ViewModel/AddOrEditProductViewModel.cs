d83f45c8664f2c03337d2b5ea39c7f8760d1c47f
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-11-06 23:33:11 +0700

    Get ViewModel from NashSneaker.API

---
+++ NashSneaker.Data/ViewModel/AddOrEditProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NashSneaker.ViewModel
+{
+    public class AddOrEditProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public List<string> Sizes { get; set; }
+        public List<string> imagesName { get; set; }
+        public List<IFormFile> imagesFile { get; set; }
+        public List<string> imagesDelete { get; set; }
+    }
+}
