3725effa57118ec68006baba170be62aae2e8ffd
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-11-09 20:37:33 +0700

    Add ViewModels for Orders Management

---
+++ NashSneaker.Data/ViewModel/EditProductFromOrderDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashSneaker.Data.ViewModel
+{
+    public class EditProductFromOrderDetailViewModel
+    {
+        public int orderDetailId { get; set; }
+        public int quantity { get; set; }
+    }
+}
