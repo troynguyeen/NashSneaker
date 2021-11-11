89771b73bd8f8091665d7d2fe18be91f8fb51ff1
Nguyễn Chí Thành <classic.nct@gmail.com>
2021-11-11 12:05:57 +0700

    Prepare to publish BlobService

---
+++ NashSneaker.BlobService/IBlobService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NashSneaker.BlobServices
+{
+    public interface IBlobService
+    {
+        Task<string> GetBlob(string name, string containerName);
+        Task<IEnumerable<string>> AllBlobs(string containerName);
+        Task<bool> UploadBlob(string name, IFormFile file, string containerName);
+        Task<bool> DeleteBlob(string name, string containerName);
+    }
+}
